698f73f6d1c4bdd4d61632f367d40943b3e6cbeb
Vincent Horvath <vhorvath2010@gmail.com>
2024-12-23 15:04:32 -0500

    basic websocket connection

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,26 @@
 import { Hono } from "hono";
-import { serveStatic } from "hono/deno";
+import { serveStatic, upgradeWebSocket } from "hono/deno";
+import { html } from "typed-htmx";
 
 const app = new Hono();
-app.use('*', serveStatic({ root: './public' }));
+app.use("*", serveStatic({ root: "./public" }));
 
-const api = new Hono();
-api.get("/hello", (c) => c.text("Hello from Hono!"));
+app.get(
+  "/ws",
+  upgradeWebSocket((_c) => {
+    return {
+      onMessage(event, ws) {
+        console.log("received message", event);
+        ws.send(html`
+          <div id="chat_room" hx-swap-oob="beforeend">
+            <p>
+              message
+            </p>
+          </div>
+          `);
+      },
+    };
+  }),
+);
 
-app.route("/api", api);
 Deno.serve(app.fetch);
